88a13557b572b141e13de911fb86e52c8b5216a4
Collins Mwichabe <mwichabecollins@gmail.com>
2022-07-12 11:34:11 +0300

    added list view to card and container

--- lib/main.dart
+++ lib/main.dart
@@ -99,121 +99,145 @@ class transaction_history extends StatelessWidget {
                 ),
               ]
             ),
+            SizedBox(height: 20),
             Row(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Container(
-                    color: Colors.white,
-                    width: 750,
-                    margin: EdgeInsets.all(16.0),
-                    padding: EdgeInsets.all(10.0),
-                    child:Row(
-                        mainAxisAlignment:MainAxisAlignment.start,
-                        children: [
-                          Expanded(
-                              flex:3,
-                              child: Text('RECEIVER',style: TextStyle(color: Colors.grey),)),
-                          Expanded(
-                            flex: 1,
-                            child: SizedBox(
-                              width: 50,
+                  color: Colors.white,
+                  width: 750,
+                  height: 400,
+                  child: ListView(
+                    scrollDirection: Axis.vertical,
+                    children: [
+                      Column(
+                          children:[
+                            Container(
+                                color: Colors.white,
+                                width: 750,
+                                margin: EdgeInsets.all(16.0),
+                                padding: EdgeInsets.all(10.0),
+                                child: Row(
+                                    mainAxisAlignment:MainAxisAlignment.start,
+                                    children: [
+                                      Expanded(
+                                          flex:3,
+                                          child: Text('RECEIVER',style: TextStyle(color: Colors.grey),)),
+                                      Expanded(
+                                        flex: 1,
+                                        child: SizedBox(
+                                          width: 50,
+                                        ),
+                                      ),
+                                      Expanded(
+                                          flex:2,
+                                          child: Text('TYPE',style: TextStyle(color: Colors.grey),)),
+                                      Expanded(
+                                        flex: 1,
+                                        child: SizedBox(
+                                          width: 30,
+                                        ),
+                                      ),
+                                      Expanded(
+                                          flex:2,
+                                          child:Text('DATE',style: TextStyle(color: Colors.grey),)),
+                                      Expanded(
+                                        flex: 1,
+                                        child: SizedBox(
+                                          width: 50,
+                                        ),
+                                      ),
+                                      Expanded(
+                                          flex: 3,
+                                          child: Text('AMOUNT',style: TextStyle(color: Colors.grey),)),
+                                      Expanded(flex: 1,
+                                        child: SizedBox(
+                                          width: 40,
+                                        ),
+                                      ),
+                                      Expanded( flex: 2,
+                                          child: Text('STATUS',style: TextStyle(color: Colors.grey),)),
+                                      SizedBox(width: 20),
+                                    ])),
+                            Divider(
+                              height: 2,
+                              color: Colors.grey,
                             ),
-                          ),
-                          Expanded(
-                              flex:2,
-                              child: Text('TYPE',style: TextStyle(color: Colors.grey),)),
-                          Expanded(
-                            flex: 1,
-                            child: SizedBox(
-                              width: 30,
-                            ),
-                          ),
-                          Expanded(
-                              flex:2,
-                              child:Text('DATE',style: TextStyle(color: Colors.grey),)),
-                          Expanded(
-                            flex: 1,
-                            child: SizedBox(
-                              width: 50,
-                            ),
-                          ),
-                          Expanded(
-                              flex: 3,
-                              child: Text('AMOUNT',style: TextStyle(color: Colors.grey),)),
-                          Expanded(flex: 1,
-                            child: SizedBox(
-                              width: 40,
-                            ),
-                          ),
-                          Expanded( flex: 2,
-                              child: Text('STATUS',style: TextStyle(color: Colors.grey),)),
-                          SizedBox(width: 20),
-                        ])),
+
+                          ]
+
+                      ),
+                    ],
+                  ),
+                ),
                 SizedBox(width: 40),
+
                 Expanded(
                   flex: 5,
-                  child:                 Container(
+                  child:Container(
                     width: 400,
                     height: 230,
-                    child: Card(
-                      child:  Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children:
-                        [
-                          Row(
-                            children: [
-                              ElevatedButton.icon
-                                (onPressed: (){},
-                                icon:Icon(Icons.refresh,color: Colors.black),
-                                label:Text(''),
-                                style: ElevatedButton.styleFrom(
-                                    primary: Colors.grey[100]
-                                ),
+                    child: ListView(
+                      children:[ Card(
+                    child:  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                      children:
+                      [
+                        Row(
+                          children: [
+                            ElevatedButton.icon
+                              (onPressed: (){},
+                              icon:Icon(Icons.refresh,color: Colors.black),
+                              label:Text(''),
+                              style: ElevatedButton.styleFrom(
+                                  primary: Colors.grey[100]
                               ),
-                              SizedBox(
-                                width: 5.0,
-                              ),
-                              Text('Conversion',style: TextStyle(fontWeight: FontWeight.bold),),
-                              Divider(
-                                height: 10,
-                                color: Colors.grey,
-                              )],
-                          ),
-                          SizedBox(
-                            height: 10.0,
-                          ),
-                          Text('Enter Amount'),
-                          SizedBox(height: 10.0,),
-                          Container(
-                            width: 210.0,
-                            height: 40.0,
-                            decoration: BoxDecoration(
-                              color: Colors.grey[400],
                             ),
-/*child:Text
-                             (
-                             'USD'
-                           ),*/
-                          ),
-                          Container(
-                            color: Colors.purpleAccent,
-                            child: IconButton(
-                                onPressed:(){},
-                                icon: Icon(Icons.repeat,color: Colors.black,)),
-                          ),
-                          Container(
+                            SizedBox(
+                              width: 5.0,
+                            ),
+                            Text('Conversion',style: TextStyle(fontWeight: FontWeight.bold),),
+                            Divider(
+                              height: 10,
+                              color: Colors.grey,
+                            )]
+                        ),
+                        SizedBox(
+                          height: 10.0,
+                        ),
+                        Text('Enter Amount'),
+                        SizedBox(height: 10.0,),
+                        Container(
+                          width: 210.0,
+                          height: 40.0,
+                          decoration: BoxDecoration(
                             color: Colors.grey[400],
-                            width: 210,
-                            height: 30,
                           ),
-                          Text('Rate= 14,840.50',style: TextStyle(fontWeight: FontWeight.bold),),
-                          SizedBox(height: 20,),
-                        ],
-                      ),
-                    ),
-                  ),
-                )],
-            ),
+/*child:Text
+                               (
+                               'USD'
+                             ),*/
+                        ),
+                        Container(
+                          color: Colors.purpleAccent,
+                          child: IconButton(
+                              onPressed:(){},
+                              icon: Icon(Icons.repeat,color: Colors.black,)),
+                        ),
+                        Container(
+                          color: Colors.grey[400],
+                          width: 210,
+                          height: 30,
+                        ),
+                        Text('Rate= 14,840.50',style: TextStyle(fontWeight: FontWeight.bold),),
+                        SizedBox(height: 20,),
+                      ]
+                    )
+                  )]
+                    )
+                  )
+                )]
+            )
 
 
           ]
